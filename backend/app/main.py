eaabd01cfa3f387e6e029db51dcabf3ea9a8fd64
Victor Hugo <vhlcads@gmail.com>
2025-12-13 16:06:00 -0300

    feat: api entrpoint with integration test

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .database import engine, Base
+from .config import settings
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI(title=settings.PROJETC_NAME)
+
+origins = [
+    "http://localhost:3000", # React default
+    "http://localhost:5173", # Vite default
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+
+def read_root():
+    return {"message":"Prototype PGO", "docs": "/docs"}
