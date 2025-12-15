af977a37ca967b2bbee4a2a8a83352c5becbcb35
Victor Hugo <vhlcads@gmail.com>
2025-12-14 22:41:54 -0300

    test: Route verification and test_model correction

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -3,7 +3,7 @@ from app import models
 from sqlalchemy import inspect
 
 def test_models_tables_exist():
-    
+    Base.metadata.drop_all(bind=engine) # It helps when there are any changes to the models.
     Base.metadata.create_all(bind=engine)
     
     inspector = inspect(engine)
