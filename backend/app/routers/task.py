9d2574bce02dba57029492ec8d727f801874429e
Victor Hugo <vhlcads@gmail.com>
2025-12-15 19:03:49 -0300

    fix: Corrections to the code syntax and additions to the route in the main function

--- backend/app/routers/task.py
+++ backend/app/routers/task.py
@@ -6,8 +6,8 @@ from ..database import get_db
 
 router = APIRouter(tags=["tasks_guidance"])
 
-@router.post("guidances/", response_model=schemas.GuidanceResponse)
+@router.post("/guidances/", response_model=schemas.GuidanceResponse)
 
 def create_guidance(guidance: schemas.GuidanceCreate, db: Session = Depends(get_db)):
-    return crud.create_guidance(db=db, guidance_date=guidance.model.dump())
+    return crud.create_guidance(db=db, guidance_date=guidance.model_dump())
 
