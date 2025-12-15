63fbde3be82d994124909a2fc73dd3c19c3f8521
Victor Hugo <vhlcads@gmail.com>
2025-12-15 17:08:14 -0300

    feat: Adds route creation guidance

---
+++ backend/app/routers/task.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from .. import crud, schemas, models
+from ..database import get_db
+
+router = APIRouter(tags=["tasks_guidance"])
+
+@router.post("guidances/", response_model=schemas.GuidanceResponse)
+
+def create_guidance(guidance: schemas.GuidanceCreate, db: Session = Depends(get_db)):
+    return crud.create_guidance(db=db, guidance_date=guidance.model.dump())
+
