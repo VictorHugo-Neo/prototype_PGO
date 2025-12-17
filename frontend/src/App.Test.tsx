4377d589132f9c75fe42e68edab0d0cf859480d2
Victor Hugo <vhlcads@gmail.com>
2025-12-16 22:58:38 -0300

    feat: setup tailwind css

--- frontend/src/App.Test.tsx
+++ frontend/src/App.Test.tsx
@@ -1,10 +1,13 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import { describe, it, expect} from "vitest";
 import App from "./App";
 
 describe('App', () => {
-    it('renders without crashing', () => {
+    it('applies tailwind classes', () => {
         render(<App />);
-        expect(document.body).toBeDefined();
+        const heading = screen.getByText(/TESTE PGO FRONTEND/i);
+        expect(heading).toHaveClass("text-3xl");
+        expect(heading).toHaveClass("text-blue-600");
+        expect(document.body).toBeDefined()
     });
 });
