31b8bd4109f9943a252a034826ceb65430651a22
VictorHugo-Neo <prfvhlc@gmail.com>
2026-02-02 22:26:19 -0300

    fix: correção de url

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://seu-projeto-vercel.app",
+    "https://prototype-pgo.vercel.app/",
     "*"
 ]
 
